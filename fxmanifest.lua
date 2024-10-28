a6fa59c444ab8f492d0774de8fb8e7b3bd8ec651
SwagDaddyBrad <bradlee.kolvoord@gmail.com>
2024-10-28 18:12:05 -0400

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -4,7 +4,7 @@ game 'gta5'
 name "CB-Farming"
 description "Fruit/Vegetable picking script for FiveM RP servers"
 author "Cool Brad Scripts"
-version "1.0.0"
+version "1.1.0"
 
 lua54 'yes'
 use_experimental_fxv2_oal 'yes'
